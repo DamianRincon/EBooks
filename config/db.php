9d2f78ea4606179e9038dc832f54be1f2bc05206
Damian Rincon <drincon@onikom.com.mx>
2021-02-11 19:47:18 -0600

    ebooks version final

--- config/db.php
+++ config/db.php
@@ -4,8 +4,8 @@
  * Archivo de configuración para los parametros de la base de datos. 
 */
 
-define('host','damianrc.com'); // cambiar a localhost o el host en el servidor
-define('password','543@:kFp9WuoLS');
-define('db_name','damianrc_ebooks'); // nombre de la base de datos
-define('user','damianrc'); // root 
+define('host',''); // cambiar a localhost o el host en el servidor
+define('password','');
+define('db_name','ebooks'); // nombre de la base de datos
+define('user',''); // root 
 ?>
--- config/sys.php
+++ config/sys.php
@@ -3,10 +3,11 @@
  * Configuración de variables del sistema
  */
 
-define('app_name' , 'Biblioteca');
+define('app_name' , 'Ebooks');
 define('primary_color' , '#b71c1c');
-define('email_user', 'damianrc.dev@gmail.com');
-define('email_password', 'DamianRC05');
+define('email_user', ''); // correo electronico 
+define('email_password', ''); // correo electronico
 define('banner_image' , 'resource/images/app/background.webp');
 define('url_base' , 'http://localhost/Ebooks/'); //https://damianrc.com/demo/Ebooks/
+
 ?>
